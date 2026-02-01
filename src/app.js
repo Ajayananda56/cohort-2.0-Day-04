0f13c349c112ad2e766c4db7417f54b6270ccc42
Ajaya Nanda <ajayananda56@gmail.com>
2026-02-01 18:31:46 +0530

    day-4 save final

--- src/app.js
+++ src/app.js
@@ -5,36 +5,35 @@ const express = require("express");
 
 const app = express();
 
-const notes =[]
+const notes = [];
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello from express server");
 });
 
-app.post("/notes", (req, res)=>{
-    console.log(req.body)
-    res.send("note received")
-    notes.push(req.body)
-    console.log(notes)
-})
+app.post("/notes", (req, res) => {
+  console.log(req.body);
+  res.send("note received");
+  notes.push(req.body);
+  console.log(notes);
+});
 
-app.get("/notes", (req, res)=>{
-    res.send(notes)
-})
+app.get("/notes", (req, res) => {
+  res.send(notes);
+});
 //: nehi pata ":"
 //DELETE .notes/2
-app.delete("/notes/:index", (req, res)=>{
-    delete notes[req.params.index]
+app.delete("/notes/:index", (req, res) => {
+  delete notes[req.params.index];
 
-    res.send("note deleted successfully")
-   
-})
+  res.send("note deleted successfully");
+});
 
 //patch / notes : index
 // req.body = {description: }
-app.patch("/notes/:index", (req, res)=>{
-    notes[req.params.index].description = req.body.description;
-notes[req.params.index].title = req.body.title;
-    res.send("note updated successfully")
-})
+app.patch("/notes/:index", (req, res) => {
+  notes[req.params.index].description = req.body.description;
+  notes[req.params.index].title = req.body.title;
+  res.send("note updated successfully");
+});
 module.exports = app;
